af5259de942ce20a687480dbe89c499c07eb1a11
Mohammed S Shurrab <m.sh@unitone.ps>
2015-10-18 14:11:01 +0300

    Modify welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 Parse Server<br/>(Baas)</div>
+                <a href="https://github.com/unitoneict/baas"><img style="position: absolute; top: 0; right: 0; border: 0; width: 149px; height: 149px;" src="http://aral.github.com/fork-me-on-github-retina-ribbons/right-red@2x.png" alt="Fork me on GitHub"></a>
             </div>
         </div>
     </body>
